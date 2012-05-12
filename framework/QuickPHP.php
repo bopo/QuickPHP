c826295eb51691aea4787c94187bbc00b9ed98db
yi.wang <ibopo@126.com>
2012-05-12 11:42:30 +0800

    Update framework/QuickPHP.php

--- framework/QuickPHP.php
+++ framework/QuickPHP.php
@@ -1,24 +1,23 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
-
-/**
- * QuickPHP Framework
- *
- * LICENSE
- *
- * This source file is subject to the new BSD license that is bundled
- * with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://framework.quickphp.net/license/new-bsd
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@quickphp.net so we can send you a copy immediately.
- *
- * @category   QuickPHP
- * @package    QuickPHP
- * @copyright  Copyright (c) 2010 http://quickphp.net All rights reserved.
- * @license    http://framework.quickphp.net/license/new-bsd     New BSD License
- * @version    $Id: QuickPHP.php 8761 2012-01-15 05:10:59Z bopo $
- */
+/*
+ +----------------------------------------------------------------------+
+ | QuickPHP Framework Version 0.10                                      |
+ +----------------------------------------------------------------------+
+ | Copyright (c) 2010 QuickPHP.net All rights reserved.                 |
+ +----------------------------------------------------------------------+
+ | Licensed under the Apache License, Version 2.0 (the 'License');      |
+ | you may not use this file except in compliance with the License.     |
+ | You may obtain a copy of the License at                              |
+ | http://www.apache.org/licenses/LICENSE-2.0                           |
+ | Unless required by applicable law or agreed to in writing, software  |
+ | distributed under the License is distributed on an 'AS IS' BASIS,    |
+ | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+ | implied. See the License for the specific language governing         |
+ | permissions and limitations under the License.                       |
+ +----------------------------------------------------------------------+
+ | Author: BoPo <ibopo@126.com>                                         |
+ +----------------------------------------------------------------------+
+*/
 
 /**
  * QuickPHP 核心类，应用程序的调度工作
