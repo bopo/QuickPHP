9fbdd43d9e5c8393a86c9e32bc08d639ced0438a
bopo <ibopo@126.com>
2012-05-17 23:33:33 +0800

    update.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -61,33 +61,9 @@ $settings = array(
 	'caching'    => true,                    // 开启高速缓存
 	'frontend'   => '',             // 入口文件名(默认为index.php)
 	'url_suffix' => 'html',
-	'domain'     => '/sites/QuickPHP/',      // 网站域名
+	'domain'     => '/quickphp/',      // 网站域名
 );
 
 QuickPHP::instance($settings);
 
-//grab the version of phpunit, OPP: phpunit provided version call
-//$version = `{$_SERVER['argv'][0]} --version`;
-//preg_match('/(\d\d?)\.(\d\d?)(\.(\d\d?))?/', $version, $matches);
-
-//if($matches[1] <= 3)
-//{
-//    if($matches[2] <= 4)
-//    {
-//        require_once 'PHPUnit/Framework.php';
-//
-//        if($matches[2] <= 3)
-//        {
-//            require_once 'PHPUnit/Framework/TestCase.php';
-//            require_once 'PHPUnit/Framework/IncompleteTestError.php';
-//            require_once 'PHPUnit/Framework/TestSuite.php';
-//        }
-//    }
-//}
 require_once 'PHPUnit/Framework.php';
-require_once 'PHPUnit/Framework/TestCase.php';
-require_once 'PHPUnit/Framework/IncompleteTestError.php';
-require_once 'PHPUnit/Framework/TestSuite.php';
-
-require_once 'PHPUnit/TextUI/TestRunner.php';
-require_once 'PHPUnit/Util/Filter.php';
